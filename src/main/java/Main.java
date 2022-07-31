b1cb983dcdb0523c3fab870e45e6dbc97d284b47
Your Name <jelena.mikelsone@gmail.com>
2022-07-31 13:29:46 +0300

    main added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import controller.MenuController;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        MenuController menuController = new MenuController();
+        JOptionPane.showMessageDialog(null,"Welcome to library!" );
+        menuController.start();
+    }
 }
